cb03fbca75e7af14c153a509ec8e1b98e23f8ca6
bekachan <bekatasi7@gmail.com>
2022-05-17 17:49:14 +0600

    склоньтесь

--- InternetShopApi/images/models.py
+++ InternetShopApi/images/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class Images(models.Model):
     image = models.ImageField(upload_to='images/', )
     created_at = models.DateTimeField(auto_now_add=True)
-    product = models.ForeignKey(Products, related_name='image', on_delete=models.CASCADE, required=True)
+    product = models.ForeignKey(Products, related_name='image', on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
 
-
+    def __str__(self):return f'image of {self.product}'
